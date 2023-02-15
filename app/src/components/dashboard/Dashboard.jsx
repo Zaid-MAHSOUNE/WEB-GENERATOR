6996fede9647e894b2b127dc6e47d46851d1ef6c
Zaid <zd.mahsoune@gmail.com>
2023-02-15 19:45:50 +0100

    add droppable containers

--- app/src/components/dashboard/Dashboard.jsx
+++ app/src/components/dashboard/Dashboard.jsx
@@ -1,30 +1,38 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { useDrop } from 'react-dnd';
 import styles from '../../assets/css/dashboard.module.css';
 import { DropItem } from './DropItem';
 import '../../assets/css/dropItem.css';
 export const Dashboard = () => {
 
+    const refItems = useRef();
+
     const [itemList, setItemList] = useState([]);
     
     const [{isOver}, drop] = useDrop(()=>({
         accept:"item",
-        drop:(item) => dropHandler(item.tag),
+        drop:(item,monitor) => dropHandler(item.tag,monitor),
         collect: (monitor) => ({
-            isOver: !!monitor.isOver(),
+            isOver: monitor.isOver(),
+            isOverCurrent: monitor.isOver({ shallow: true }),
         })
-    })); 
+    }));
 
-    const dropHandler = (tag)=>{
+    const dropHandler = (tag,monitor)=>{
+        refItems.current = drop.current;
+        const didDrop = monitor.didDrop()
+        if (didDrop) {
+          return
+        }
         setItemList((previousState)=>{
             return [...previousState,tag];
         });
     }
 
     return (
-        <div ref={drop}  id="Drop" className={styles.container}>
+        <div ref={drop} className={styles.container}>
             {
-                itemList.map((element)=><DropItem tag={element}/>)
+                itemList.map((element,index)=><DropItem id={index} tag={element}/>)
             }
         </div>
     );
--- app/src/components/dashboard/DropItem.jsx
+++ app/src/components/dashboard/DropItem.jsx
@@ -1,4 +1,42 @@
 import '../../assets/css/dropItem.css';
-export const DropItem = (props) =>  <props.tag className='container'onClick={()=>{
-        
-}}/>
+import { useState,useRef } from 'react';
+import { useDrop } from 'react-dnd';
+export const DropItem = (props) =>  {
+
+    const parentIndex = useRef(props.id);
+
+    const [itemLists, setItemList] = useState([]);
+    
+    const [{isOver}, drop] = useDrop(()=>({
+        accept:"item",
+        drop:(item,monitor) => dropHandler(item.tag,monitor),
+        collect: (monitor) => ({
+            isOver: monitor.isOver(),
+            isOverCurrent: monitor.isOver({ shallow: true }),
+        })
+    }));
+
+    const dropHandler = (tag,monitor)=>{
+        const didDrop = monitor.didDrop()
+        if (didDrop) {
+          return
+        }
+        setItemList((previousState)=>{
+            return [...previousState,tag];
+        });
+    }
+
+    if(props.tag !== "div"){
+        return (
+            <props.tag id={props.id} className='container'/>
+        );
+    }else{
+        return (
+            <div ref={drop} id={props.id} className='container'> 
+                {
+                    itemLists.map((element,index)=><DropItem id={parentIndex.current+"-"+index} tag={element}/>)
+                }
+            </div>
+        );
+    }
+}
